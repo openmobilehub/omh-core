33eb2ad947a25511f77a8960339da33d966bc12c
Anton Soares <asoares@butterflynetinc.com>
2023-05-04 12:37:21 -0500

    modified Service.

--- omh-core/src/main/java/com/omh/android/coreplugin/model/Service.kt
+++ omh-core/src/main/java/com/omh/android/coreplugin/model/Service.kt
@@ -5,20 +5,34 @@ import org.gradle.api.provider.Property
 import javax.inject.Inject
 
 open class Service @Inject constructor(project: Project) {
-    private val gms: Property<String> = project.objects.property(String::class.java)
-    private val nonGms: Property<String> = project.objects.property(String::class.java)
 
-    internal fun isThereGmsService() = gms.isPresent && gms.get().trim().isNotEmpty()
-    internal fun isThereNonGmsService() = nonGms.isPresent && nonGms.get().trim().isNotEmpty()
+    private val gmsProperty: Property<String> = project.objects.property(String::class.java)
+    private val gmsPathProperty: Property<String> = project.objects.property(String::class.java)
 
-    fun addGmsService(groupIdWithVersion: String) {
-        gms.set(groupIdWithVersion)
+    private val nonGmsProperty: Property<String> = project.objects.property(String::class.java)
+    private val nonGMSPathProperty: Property<String> = project.objects.property(String::class.java)
+
+    internal fun isThereGmsService(): Boolean {
+        return gmsProperty.isPresent && gmsProperty.get().trim().isNotEmpty()
     }
 
-    fun addNonGmsService(groupIdWithVersion: String) {
-        nonGms.set(groupIdWithVersion)
+    internal fun isThereNonGmsService(): Boolean {
+        return nonGmsProperty.isPresent && nonGmsProperty.get().trim().isNotEmpty()
     }
 
-    internal fun gmsService() = gms.get()
-    internal fun nonGmsService() = nonGms.get()
+    fun setGmsService(groupIdWithVersion: String, reflectionPath: String) {
+        gmsProperty.set(groupIdWithVersion)
+        gmsPathProperty.set(reflectionPath)
+    }
+
+    fun setNonGmsService(groupIdWithVersion: String, reflectionPath: String) {
+        nonGmsProperty.set(groupIdWithVersion)
+        nonGMSPathProperty.set(reflectionPath)
+    }
+
+    internal fun gmsService() = gmsProperty.get()
+    internal fun nonGmsService() = nonGmsProperty.get()
+    internal fun getGmsPath() = gmsPathProperty.get()
+    internal fun getNonGmsPath() = nonGMSPathProperty.get()
+
 }
