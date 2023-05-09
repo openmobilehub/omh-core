7401102022ca5479154ad8c7505743bb85ead4a1
Anton Tchistiakova <atch97@gmail.com>
2023-05-09 11:48:07 -0500

    Added BuildConfigField generation for reflection paths of the modules. (#4)
    ---------
    
    Co-authored-by: Anton Soares <asoares@butterflynetinc.com>
    Co-authored-by: Juan Hoyos <jdiegoh3@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,14 @@ subprojects {
         mavenLocal()
     }
 }
+
+tasks.register("installPreCommitHook", Copy::class) {
+    from("tools/scripts/pre-commit")
+    into(".git/hooks")
+    fileMode = 0b000_111_111_111
+}
+
+tasks {
+    val installPreCommitHook by existing
+    getByName("prepareKotlinBuildScriptModel").dependsOn(installPreCommitHook)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,5 +16,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "OMH"
-
 include(":omh-core")
