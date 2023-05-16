60fe2eb8f1a5198f6ae7419d39860126db2ba1e6
Anton Tchistiakova <anton.soares@encora.com>
2023-05-16 12:47:39 -0500

    Removed the default config and disabled the original buildTypes. (#5)

--- omh-core/src/main/java/com/omh/android/coreplugin/utils/ApiPath.kt
+++
@@ -1,7 +0,0 @@
-package com.omh.android.coreplugin.utils
-
-object ApiPath {
-    internal const val AUTH = "com.omh.android:auth:1.0-SNAPSHOT"
-    internal const val MAPS = "com.omh.android:maps-api:1.0-SNAPSHOT"
-    internal const val STORAGE = "com.omh.android:storage-api:1.0-SNAPSHOT"
-}
